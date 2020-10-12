29687b27ca78f42f1e9d9eb258657299607ba492
Nicoleta Sarosi <sarosinicoleta08@gmail.com>
2020-10-12 14:02:30 +0300

    My Ionic App

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div className="container">
+      <strong>HELLO WORLD</strong>
+    </div>
+  );
+};
+
+export default ExploreContainer;
